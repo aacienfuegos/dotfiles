2fec5318206410685e42bed10b7511b893767658
aacienfuegos <andres.acienfuegos@gmail.com>
2021-08-24 20:40:21 +0200

    fix: html lsp

--- nvim/lua/aacienfuegos/lsp/html.lua
+++ nvim/lua/aacienfuegos/lsp/html.lua
@@ -1,4 +1,8 @@
 -- requires snippet support to provide completion
+local nvim_lsp = require('lspconfig')
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true;
+
 nvim_lsp.html.setup {
     capabilities = capabilities;
     on_attach = on_attach,
--- nvim/lua/aacienfuegos/lsp/init.lua
+++ nvim/lua/aacienfuegos/lsp/init.lua
@@ -1,9 +1,5 @@
 require'snippets'.use_suggested_mappings()
 
-local nvim_lsp = require('lspconfig')
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true;
-
 require('aacienfuegos.lsp.bashls')
 require('aacienfuegos.lsp.cmake')
 require('aacienfuegos.lsp.html')
