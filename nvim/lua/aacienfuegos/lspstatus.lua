1dda978b839d715d9fef8ba810da5c42025c4517
Andres Cienfuegos <andres.acienfuegos@gmail.com>
2021-10-22 15:35:26 +0200

    capabilities set here

--- nvim/lua/aacienfuegos/lspstatus.lua
+++ nvim/lua/aacienfuegos/lspstatus.lua
@@ -1,3 +1,8 @@
 local lsp_status = require('lsp-status')
 
+-- LSP
+local updated_capabilities = vim.lsp.protocol.make_client_capabilities()
+updated_capabilities = vim.tbl_deep_extend("keep", updated_capabilities, lsp_status.capabilities)
+require('lspconfig').util.default_config.capabilities = updated_capabilities
+
 lsp_status.register_progress()
