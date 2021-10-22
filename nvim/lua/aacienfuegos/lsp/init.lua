3f587b0c2246ac252afc76819137d7bf74047ac9
Andres Cienfuegos <andres.acienfuegos@gmail.com>
2021-10-22 15:34:41 +0200

    css ls config

--- nvim/lua/aacienfuegos/lsp/init.lua
+++ nvim/lua/aacienfuegos/lsp/init.lua
@@ -2,6 +2,7 @@ require'snippets'.use_suggested_mappings()
 
 require('aacienfuegos.lsp.bashls')
 require('aacienfuegos.lsp.cmake')
+require('aacienfuegos.lsp.css')
 require('aacienfuegos.lsp.html')
 require('aacienfuegos.lsp.pyright')
 require('aacienfuegos.lsp.rust_analyzer')
