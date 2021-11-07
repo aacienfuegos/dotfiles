01ed6ea45e80b2b02882c32dea425b2650cf7aca
Andres Cienfuegos <andres.acienfuegos@gmail.com>
2021-11-07 03:26:32 +0100

    copilot insert with <c-i>

--- nvim/lua/aacienfuegos/copilot.lua
+++ nvim/lua/aacienfuegos/copilot.lua
@@ -1,4 +1,4 @@
 vim.g.copilot_assume_mapped = true
 vim.g.copilot_no_tab_map = true
 
-vim.api.nvim_set_keymap('i', '<C-J>', 'copilot#Accept("\\<CR>")', { silent = true, script = true, expr = true })
+vim.api.nvim_set_keymap('i', '<C-I>', 'copilot#Accept("\\<CR>")', { silent = true, script = true, expr = true })
