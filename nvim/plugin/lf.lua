161960242df6c1143caa9f59b42ff1001678cd28
Andres Cienfuegos <andres.acienfuegos@gmail.com>
2022-01-20 18:51:56 +0100

    lf opens with lfrun

--- nvim/plugin/lf.lua
+++ nvim/plugin/lf.lua
@@ -1,6 +1,7 @@
+vim.g.lf_command_override = "lfrun"
 vim.g.lf_map_keys = 0
 vim.api.nvim_set_keymap("n", "<leader>o", ":LfCurrentFile<CR>", {noremap = true, silent = true})
 
 -- " Open lf instead of NERDTree
-vim.g.NERDTreeHijackNetrw = 0
+-- vim.g.NERDTreeHijackNetrw = 0
 vim.g.lf_replace_netrw = 1
