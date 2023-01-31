1e55fff54771fdaf2a7a97a27b5c30ba18fdce48
Andres Cienfuegos <andres.acienfuegos@gmail.com>
2023-01-31 18:38:52 +0100

    remove deprecated options for opening nvim-tree on startup

--- nvim/lua/aacienfuegos/nvimtree/init.lua
+++ nvim/lua/aacienfuegos/nvimtree/init.lua
@@ -1,9 +1,6 @@
 require 'nvim-tree'.setup {
 	disable_netrw       = true,
 	hijack_netrw        = true,
-	open_on_setup       = false,
-	-- ignore_ft_on_setup  = { '.git', 'node_modules', '.cache' },
-	ignore_ft_on_setup  = { 'startify', 'dashboard' },
 	hijack_directories  = {
 		enable = true,
 		auto_open = true,
