1923a3014893f7ae79566b105177aa2ed58f486c
Andres Cienfuegos <andres.acienfuegos@gmail.com>
2023-12-29 18:24:38 +0100

    reformat nvim gitsigns config

--- nvim/lua/aacienfuegos/gitsigns.lua
+++ nvim/lua/aacienfuegos/gitsigns.lua
@@ -1,8 +1,3 @@
-local ok, signs = pcall(require, "gitsigns")
-if not ok then
-  return
-end
-
 require "colorbuddy"
 
 local c = require("colorbuddy.color").colors
@@ -12,48 +7,87 @@ Group.new("GitSignsAdd", c.green)
 Group.new("GitSignsChange", c.yellow)
 Group.new("GitSignsDelete", c.red)
 
-signs.setup {
-  signs = {
-    add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr" },
-    change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr" },
-    delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr" },
-    topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr" },
-    changedelete = { hl = "GitSignsDelete", text = "~", numhl = "GitSignsChangeNr" },
-  },
+require('gitsigns').setup {
+	signs                        = {
+		add          = { text = '│' },
+		change       = { text = '│' },
+		delete       = { text = '_' },
+		topdelete    = { text = '‾' },
+		changedelete = { text = '~' },
+		untracked    = { text = '┆' },
+	},
+	signcolumn                   = true, -- Toggle with `:Gitsigns toggle_signs`
+	numhl                        = true, -- Toggle with `:Gitsigns toggle_numhl`
+	linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
+	word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
+	watch_gitdir                 = {
+		follow_files = true
+	},
+	attach_to_untracked          = true,
+	current_line_blame           = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+	current_line_blame_opts      = {
+		virt_text = true,
+		virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+		delay = 1000,
+		ignore_whitespace = false,
+		virt_text_priority = 100,
+	},
+	current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+	sign_priority                = 6,
+	update_debounce              = 100,
+	status_formatter             = nil, -- Use default
+	max_file_length              = 40000, -- Disable if file is longer than this (in lines)
+	preview_config               = {
+		-- Options passed to nvim_open_win
+		border = 'single',
+		style = 'minimal',
+		relative = 'cursor',
+		row = 0,
+		col = 1
+	},
+	yadm                         = {
+		enable = false
+	},
 
-  -- Highlights just the number part of the number column
-  numhl = true,
+	-- Keymaps
+	on_attach                    = function(bufnr)
+		local gs = package.loaded.gitsigns
 
-  -- Highlights the _whole_ line.
-  --    Instead, use gitsigns.toggle_linehl()
-  linehl = false,
+		local function map(mode, l, r, opts)
+			opts = opts or {}
+			opts.buffer = bufnr
+			vim.keymap.set(mode, l, r, opts)
+		end
 
-  -- Highlights just the part of the line that has changed
-  --    Instead, use gitsigns.toggle_word_diff()
-  word_diff = false,
+		-- Navigation
+		map('n', ']c', function()
+			if vim.wo.diff then return ']c' end
+			vim.schedule(function() gs.next_hunk() end)
+			return '<Ignore>'
+		end, { expr = true })
 
-  keymaps = {
-    -- Default keymap options
-    noremap = true,
-    buffer = true,
+		map('n', '[c', function()
+			if vim.wo.diff then return '[c' end
+			vim.schedule(function() gs.prev_hunk() end)
+			return '<Ignore>'
+		end, { expr = true })
 
-    ["n <leader>gn"] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'" },
-    ["n <leader>gp"] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'" },
+		-- Actions
+		map('n', '<leader>hs', gs.stage_hunk)
+		map('n', '<leader>hr', gs.reset_hunk)
+		map('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+		map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+		map('n', '<leader>hS', gs.stage_buffer)
+		map('n', '<leader>hu', gs.undo_stage_hunk)
+		map('n', '<leader>hR', gs.reset_buffer)
+		map('n', '<leader>hp', gs.preview_hunk)
+		map('n', '<leader>hb', function() gs.blame_line { full = true } end)
+		map('n', '<leader>tb', gs.toggle_current_line_blame)
+		map('n', '<leader>hd', gs.diffthis)
+		map('n', '<leader>hD', function() gs.diffthis('~') end)
+		map('n', '<leader>td', gs.toggle_deleted)
 
-    -- ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-    -- ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-    -- ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-    -- ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-    -- ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
-  },
-
-  current_line_blame_opts = {
-    delay = 2000,
-    virt_text_pos = "eol",
-  },
+		-- Text object
+		map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+	end
 }
-
--- TODO: decide mappings for this
--- local nnoremap = vim.keymap.nnoremap
--- nnoremap { "<space>tsw", signs.toggle_word_diff }
--- nnoremap { "<space>tsh", signs.toggle_word_diff }
